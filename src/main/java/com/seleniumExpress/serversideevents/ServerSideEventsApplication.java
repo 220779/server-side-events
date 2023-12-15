ca4f1a9f8fd33d01bf3daed67235ec57228b5c05
220779 <102145019+220779@users.noreply.github.com>
2023-12-15 16:53:12 +0200

    [add] Loading container (Application context)

--- src/main/java/com/seleniumExpress/serversideevents/ServerSideEventsApplication.java
+++ src/main/java/com/seleniumExpress/serversideevents/ServerSideEventsApplication.java
@@ -1,7 +1,11 @@
 package com.seleniumExpress.serversideevents;
 
+import com.seleniumExpress.serversideevents.config.AppConfig;
+import com.seleniumExpress.serversideevents.publisher.WebCafePublisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class ServerSideEventsApplication {
@@ -9,5 +13,9 @@ public class ServerSideEventsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerSideEventsApplication.class, args);
 	}
+
+	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	WebCafePublisher bean = context.getBean("webCafePublisher", WebCafePublisher.class);
+
 
 }
